a0f9f382fd827e421908546a2ff8d17f00ee7bd1
Timothy Forbes <timothy.forbes259@gmail.com>
2020-09-07 10:37:43 +0930

    homework four start

---
+++ Assets/script.js
@@ -0,0 +1,8 @@
+//Which set of characters is used to define an array?
+var arrayQuestion = ['()','[]','{}','<>']
+//Which of the following statement is not an object?
+var objectQuestion = ['var car = {make: Ford, model: Fiesta}', '[blue, white, yellow]', 'jkfds;jklfsdaj;lkfdsa;jlkdfs;jl', 'ninety-nine']
+var scriptSrcQuestion = ['']
+var apiQuestion = ['']
+//Which of the following returns true
+var booleanQuestion = ['NaN', 'undefined', '0', '1']
