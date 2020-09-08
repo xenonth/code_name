24f47de176110673cdf2e03a1c0898b9cf385f9e
Timothy Forbes <timothy.forbes259@gmail.com>
2020-09-08 13:08:08 +0930

    I have gotten the button list working

--- webpages/arrayList.js
+++ webpages/arrayList.js
@@ -5,7 +5,7 @@ window.onload = function() {
  
     //Which set of characters is used to define an array?
 
-    //Which of the following statement is not an object?
+    //Which of the following is not an object?
     //var objectQuestion = ['var car = {make: Ford, model: Fiesta}', '[blue, white, yellow]', 'jkfds;jklfsdaj;lkfdsa;jlkdfs;jl', 'ninety-nine'];
     //Which following command allows javascript to run in html?
     //var scriptSrcQuestion = ['style', 'script', 'python', 'code'];
@@ -13,29 +13,38 @@ window.onload = function() {
     //var apiQuestion = ['experiment until the application breaks', 'programmer tests for flaws', 'release without testing and wait for user feedback', 'none of the above'];
     //Which of the following returns true
     //var booleanQuestion = ['NaN', 'undefined', '0', '1'];
-
-    
-    
-//Question Variable Buttons
-
-//setting ul inside chracter ID
-//Create a new div    
-    
-   listQuizArrayQuestion (); 
+      
     
     //Listing Variables
-    //purpose is to create a ul element and attach it to the 
-    function listQuizArrayQuestion() {
-    var targetElement= document.getElementById('question');
+    //purpose is to create a quiz answer list by using the arrayQuestion Array and having buttion inside of the li Icon.
+    function listQuizArrayQuestionOne() {
+    var targetQuestionOne= document.getElementById('questionOne');
+    
     var ul = document.createElement('ul')
-        targetElement.appendChild(ul)
+        targetQuestionOne.appendChild(ul)
+
+
       var arrayQuestion = ['()','[]','{}','<>'];
+
       for (i = 0; i < arrayQuestion.length; i++) {
-          const arrayList= document.createElement("li");
+        //var addBtn = arrayList.
+        //anchor to the next page
+        var createLink = document.createElement('a');
+        createLink.setAttribute("href", "questionTwo.html");
+        //button link
+        var createBtn = document.createElement('button');
+        var arrayList= document.createElement("li");
           var list_text = document.createTextNode(arrayQuestion[i]);
-          arrayList.appendChild(list_text);
+          //appending all the elements to the ul element
+
+          createLink.appendChild(list_text);
+          createBtn.appendChild(createLink);          
+          arrayList.appendChild(createBtn);
           ul.appendChild(arrayList);
       }
 
       }
+
+      listQuizArrayQuestionOne ();
+
 }
